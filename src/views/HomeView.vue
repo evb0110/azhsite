6c2a93d296ed2945a039bec9ea0649da10d9e53b
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2022-09-10 14:51:49 +0300

    Add decorators and pics

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <hello-world />
+    <h1 class="title">
+        This is Andrey and Zhenya's page
+
+    </h1>
+    <div class="faces">
+        <img
+            v-for="(src, i) in faces"
+            :src="src"
+            :alt="src"
+            :key="i"
+            class="my-3"
+            height="200"
+        />
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { Options, Vue } from 'vue-property-decorator';
+import zhenya from '@/assets/zhenya.jpg';
+import andrey from '@/assets/andrey.jpg';
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
 
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
-});
+@Options({})
+export default class HomeView extends Vue {
+    public zhenya = zhenya;
+    public andrey = andrey;
+    public faces = [this.andrey, this.zhenya];
+}
 </script>
+
+<style lang="scss">
+.title {
+    text-align: center;
+}
+.faces {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
