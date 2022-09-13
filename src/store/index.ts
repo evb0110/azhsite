91bfef095174d93950f95234b4f48ebf0c392282
Eugene Barsky <7149553@gmail.com>
2022-09-13 22:13:41 +0300

    small changes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,10 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {},
+    strict: process.env.NODE_ENV !== 'production',
+});
